b04f96258325c3a3590d053698c6d70de1fa2d09
Tin Nguyen <tinnguyen263@outlook.com>
2019-04-29 11:47:53 +0700

    Seperate modules

--- __tests__/note.test.js
+++ __tests__/note.test.js
@@ -1,30 +1,35 @@
-const SERVER = require('./../app')
-
+const createServer = require('./../src').createServer;
 const request = require('supertest')
 
 describe('Note CRUD operation', () => {
+    let server;
+
+    before(done => {
+        server = createServer(done);
+    })
+
     it(`can get all notes`, done => {
-        request(SERVER)
+        request(server)
             .get('/note/')
             .expect(200, done)
     })
     it(`can get specific note`, done => {
-        request(SERVER)
+        request(server)
             .get('/note/')
             .expect(200, done)
     })
     it(`create a note`, done => {
-        request(SERVER)
+        request(server)
             .post('/note/')
             .expect(200, done)
     })
     it(`update a note`, done => {
-        request(SERVER)
+        request(server)
             .put('/note/')
             .expect(200, done)
     })
     it(`can delete a note`, done => {
-        request(SERVER)
+        request(server)
             .delete('/note/')
             .expect(200, done)
     })
---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+const createServer = (onCreateServer) => {
+    const express = require('express');
+    const path = require('path');
+    const cookieParser = require('cookie-parser');
+    // const logger = require('morgan');
+    
+    const app = express();
+    
+    // app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+    
+    const noteRouter = require('./routes/note')
+    app.use('/note', noteRouter);
+
+    onCreateServer && onCreateServer();
+
+    return app;
+}
+
+module.exports = {createServer}
